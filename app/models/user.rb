ea953c8428bb853d3cf36c31736036a16529dc9e
Colin Spangelo <colin.spangelo@gmail.com>
2023-09-26 17:13:26 -0600

    Added testing for user authentication, for no duplicates, ensuring whitespaces before and after email while logging in will still log you in. And a few others.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 7 }
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by(email: email.strip.downcase)
+
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,129 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'is valid with matching password and password_confirmation' do
-    user = User.new(
-      email: 'test@example.com',
+  describe 'Validations' do
+    it 'is valid with matching password and password_confirmation' do
+      @user = User.create(
+        firstname: 'Bob',
+        lastname: 'Joe',
+        email: 'test@test.com',
+        password: 'password',
+        password_confirmation: 'password'
+      )
+    expect(@user.password).to eq(@user.password_confirmation)
+    end
+
+    it 'is not valid if the password and password_confirmation is not the same value.' do
+      @user = User.create(
+        firstname: 'Bob',
+        lastname: 'Joe',
+        email: 'test@test.com',
+        password: 'password',
+        password_confirmation: 'not-password'
+      )
+    expect(@user).not_to be_valid
+    end
+
+    it 'does not save if there is no email inputted.' do
+      @user = User.create(
+        firstname: 'Bob',
+        lastname: 'Joe',
+        email: nil,
+        password: 'password',
+        password_confirmation: 'password'
+      )
+      expect(@user).not_to be_valid
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+
+    it 'does not save if last name is not inputted.' do
+      @user = User.create(
+        firstname: nil,
+        lastname: 'Joe',
+        email: 'test@test.com',
+        password: 'password',
+        password_confirmation: 'password'
+      )
+      expect(@user).not_to be_valid
+    end
+
+    it 'does not save if first name is not inputted.' do
+      @user = User.create(
+        firstname: 'Bob',
+        lastname: nil,
+        email: 'test@test.com',
+        password: 'password',
+        password_confirmation: 'password'
+      )
+      expect(@user).not_to be_valid
+    end
+    
+    it 'does not save if a user with that email already exists regardless of case' do
+      @user1 = User.create(
+        firstname: 'Bob',
+        lastname: 'Joe',
+        email: 'test@test.com',
+        password: 'password',
+        password_confirmation: 'password'
+      )
+      @user2 = User.create(
+        firstname: 'Bob',
+        lastname: 'Joe',
+        email: 'TEST@test.com',
+        password: 'password',
+        password_confirmation: 'password'
+      )
+
+      expect(@user2).not_to be_valid
+      expect(@user2.errors.full_messages).to include("Email has already been taken")
+    end
+
+    it 'does not save if the password is less than 7 characters' do
+      @user1 = User.create(
+        firstname: 'Bob',
+        lastname: 'Joe',
+        email: 'testing@test.com',
+        password: 'password',
+        password_confirmation: 'password'
+      )
+      @user2 = User.create(
+        firstname: 'Bob',
+        lastname: 'Joe',
+        email: 'testing1@test.com',
+        password: '2short',
+        password_confirmation: 'password'
+      )
+      expect(@user1).to be_valid
+      expect(@user2).not_to be_valid
+    end
+  end
+
+  describe '.authenticate_with_credentials' do 
+    it 'returns a user instance if email and password are correct.' do
+    @user = User.create(
+      firstname: 'Bob',
+      lastname: 'Joe',
+      email: 'test@test.com',
       password: 'password',
       password_confirmation: 'password'
     )
-    expect(user).to be_valid
+    user_authenticated = User.authenticate_with_credentials('test@test.com', 'password')
+    expect(user_authenticated.email).to eq(@user.email)
   end
 
+  it 'returns valid if the user has whitespaces before or after their email.' do
+    @user = User.create(
+      firstname: 'Bob',
+      lastname: 'Joe',
+      email: 'test5@test.com',
+      password: 'password',
+      password_confirmation: 'password'
+    )
+    user_authenticated = User.authenticate_with_credentials(' test5@test.com ', 'password')
 
-  
+    expect(user_authenticated).to eq(@user)
+
+  end
+end
+
 end
